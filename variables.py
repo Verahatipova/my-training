9859c818c64f18998000f650295a6c3462d61ba0
Verahatipova <ericacayher@gmail.com>
2024-12-23 11:31:51 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+course="Python"
+total_tasks=12
+hours=1.5
+execution_time="0.125 hours"
+print(course,"всего задач:", total_tasks, "затрачено часов:", hours,"среднее время выполнения:",execution_time)
